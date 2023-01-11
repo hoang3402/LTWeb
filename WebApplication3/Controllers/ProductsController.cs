47b190b197ef479cc225df6b4dd10c2e2a6490b3
Hoang3409 <nvh.02021995@gmail.com>
2023-01-11 22:16:59 +0700

    init

---
+++ WebApplication3/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using CPT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CPT.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Cách của thầy
+        public ActionResult ListProductFromCategory(int id){
+            var proCategory = ProductModel.list_ProductFormCategory(id);
+            return View(proCategory);
+        }
+    }
+}
