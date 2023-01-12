a09e7c9e677f7ff60b73396ec9ec31cf218a0221
Hoang3409 <nvh.02021995@gmail.com>
2023-01-12 08:17:09 +0700

    Done lab 4

---
+++ WebApplication3/Controllers/Customer/CustomersController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers.Customer
+{
+    public class CustomersController : Controller
+    {
+        EShopEntities db = new EShopEntities();
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(FormCollection collection)
+        {
+            var fullname = collection["Fullname"];
+            var email = collection["Email"];
+            var password = collection["Password"];
+            if (string.IsNullOrEmpty(fullname))
+            {
+                ViewData["Loi1"] = "Please Type FullName";
+            }
+            else if (string.IsNullOrEmpty(email))
+            {
+                ViewData["Loi2"] = "Please Type Email";
+            }
+            else if (string.IsNullOrEmpty(password))
+            {
+                ViewData["Loi3"] = "Please Type Password";
+            }
+            else
+            {
+                WebApplication3.Customer customer = new WebApplication3.Customer();
+                customer.Fullname = fullname;
+                customer.Email = email;
+                customer.Password = password;
+                customer.Id = CustomerModel.GetID();
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("login");
+            }
+            return this.Register();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Login(FormCollection collection)
+        {
+            var emaillg = collection["Email"];
+            var passwordlg = collection["Password"];
+            if (String.IsNullOrEmpty(emaillg))
+            {
+                ViewData["Loi1"] = "Please Type Email";
+            }
+            else if (String.IsNullOrEmpty(passwordlg))
+            {
+                ViewData["Loi1"] = "Please Type Password";
+            }
+            else
+            {
+                WebApplication3.Customer customer = db.Customers.SingleOrDefault(n => n.Email == emaillg && n.Password == passwordlg);
+                if (customer != null)
+                {
+                    ViewBag.ThongBao = "Login Success";
+                    Session["User"] = customer;
+                }
+                else
+                    ViewBag.ThongBao = "Login Faile \n Please Type Email Or Password";
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Models/CustomerModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication3.Models
+{
+    public class CustomerModel
+    {
+        static EShopEntities db = new EShopEntities();
+
+        public static int GetID()
+        {
+            List<Customer> list = db.Customers.ToList();
+            for (int i = 0; i < list.Count() - 1; i++)
+            {
+                if (list[i].Id++ != list[i++].Id)
+                {
+                    return list[i].Id++;
+                }
+            }
+            return list.Count + 1;
+        }
+    }
+}
